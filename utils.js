9fca23d4f493ded04416f4a884debea42b247ce2
Airels <yohan13240@hotmail.fr>
2020-04-05 17:55:56 +0200

    ajout fonction array -> string

--- utils.js
+++ utils.js
@@ -3,3 +3,13 @@
 exports.getRandomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
+exports.arrayToString = (array) => {
+    var string = "";
+
+    array.forEach(e => {
+        string += e + ",";
+    });
+
+    return string.substring(0, string.length-1);
+}
